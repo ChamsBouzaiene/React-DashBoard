e2ccf7c7f49e359a3e41172170a015c31af2bd22
alchemist107 <alchemist107@gmail.com>
2020-01-15 08:45:19 -0800

    feat created the toolbar title for flights table 🔧

--- src/features/Hotels/HotelToolBar.js
+++ src/features/Hotels/HotelToolBar.js
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function HotelToolBar() {
-  return <div className="hotel-toolbar">Tool Bar</div>;
+  return (
+    <div className="hotel-toolbar">
+      <div className="hotel-toolbar-title">
+        <span className="hotel-toolbar-title-contry">Moscow: </span>
+        <span className="hotel-toolbar-title-population">
+          4.002 propirties found
+        </span>
+      </div>
+      <div className="hotel-toolbar-filter">
+        <span className="hotel-toolbar-filter-by">Search By:</span>
+        <span className="hotel-toolbar-filter-selected"> recommadtions ></span>
+      </div>
+    </div>
+  );
 }
