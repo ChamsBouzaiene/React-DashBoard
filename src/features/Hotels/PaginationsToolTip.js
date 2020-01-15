c8c00ef58c7ea94deba11608ad0b10744074b958
alchemist107 <alchemist107@gmail.com>
2020-01-15 09:10:07 -0800

    feat: built hotel pagination toolbar 🔧

--- src/features/Hotels/PaginationsToolTip.js
+++ src/features/Hotels/PaginationsToolTip.js
@@ -1,5 +1,20 @@
 import React from "react";
 
 export default function PaginationsToolTip() {
-  return <div className="hotel-pagination-toolbar">pagination Tool tip</div>;
+  return (
+    <div className="hotel-pagination-toolbar">
+      <span className="pagination-element arrow">←</span>
+      <div className="pages-counter">
+        <span className="pagination-element">1</span>
+        <span className="pagination-element">2</span>
+        <span className="pagination-element">3</span>
+        <span className="pagination-element">4</span>
+        <span className="pagination-element">5</span>
+        <span className="pagination-element">6</span>
+        <span className="pagination-element">7</span>
+        <span className="pagination-element">8</span>
+      </div>
+      <span className="pagination-element arrow">→</span>
+    </div>
+  );
 }
