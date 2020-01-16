3bdcaf803c6137f6732e1c1a7320e03c7f4549e3
alchemist107 <alchemist107@gmail.com>
2020-01-15 17:00:13 -0800

    feat: created point slider mecanisme 💡

---
+++ src/features/Hotels/Hotelfilter.js
@@ -0,0 +1,47 @@
+import React, { useRef, useEffect, useState } from "react";
+
+export const Ball = () => {
+  let [offsetX, setoffsetX] = useState(0);
+  let [width, setWidth] = useState(0);
+  const ref = useRef(null);
+
+  const calculateNewPos = (width, xpos, e) => {
+    let rect = e.target.getBoundingClientRect();
+    var x = e.clientX - rect.left; //x position within the element.
+    console.log(width, xpos, x);
+    setoffsetX(x);
+  };
+  useEffect(() => {
+    const width = ref.current ? ref.current.offsetWidth : 0;
+    setWidth(width);
+    console.log("width", width);
+  }, [ref.current]);
+
+  return (
+    <div
+      className="slider-container"
+      ref={ref}
+      onClick={e => calculateNewPos(width, e.pageX, e)}
+    >
+      <div className="wrapper" style={{ left: offsetX }} />
+    </div>
+  );
+};
+
+export default function Hotelfilter() {
+  return (
+    <div className="hotel-filter-container">
+      <div className="filter-title">
+        <span>Filter by:</span>
+      </div>
+      <div className="filters-container">
+        <Ball />
+        <span>Filter by:</span>
+        <span>Filter by:</span>
+        <span>Filter by:</span>
+        <span>Filter by:</span>
+        <span>Filter by:</span>
+      </div>
+    </div>
+  );
+}
--- src/features/Hotels/index.js
+++ src/features/Hotels/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./style.css";
 import HotelsList from "./HotelsList";
 import HotelToolBar from "./HotelToolBar";
+import Hotelfilter from "./Hotelfilter";
 import PaginationsToolTip from "./PaginationsToolTip";
 
 export default function Hotels() {
@@ -9,7 +10,7 @@ export default function Hotels() {
     <div className="hotels">
       <div className="hotels-container">
         <div className="hotels-cmd">
-          <span>Best Value Offers to Europe!</span>
+          <Hotelfilter />
         </div>
         <div className="hotels-table">
           <HotelToolBar />
